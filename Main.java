c628fee61e4c4782b4dcc203e20651c4e29f7895
yoland68 <yoland68@gmail.com>
2016-06-23 00:13:50 -0700

    Updates

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main (String[] args) {
         A<C> a = new A<C>(new C());
+        A a2 = new A<C>(new C());
+        A<B> a3 = new A<B>(new C());
         System.out.println(a.toString());
+        System.out.println(a2.toString());
+        System.out.println(a3.toString());
     }
 
     static class A<T> {
